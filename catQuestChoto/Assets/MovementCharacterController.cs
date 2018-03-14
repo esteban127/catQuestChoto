5f80705a76231f02c5a53ae2c562621cfec086b4
Esteban <Esteban_127@hotmail.com>
2018-03-14 12:13:48 -0300

    changed Movement Controller

---
+++ catQuestChoto/Assets/MovementCharacterController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCharacterController : MonoBehaviour {
+
+
+    [SerializeField]
+    private float rotateSpeed = 6.0f;
+    [SerializeField]
+    private float speed = 6.0f;
+    [SerializeField]
+    private float jumpSpeed = 8.0f;
+    [SerializeField]
+    private float gravity = 20.0f;
+    [SerializeField]
+    private int maxJumpCap = 1;
+
+    private int currentJumpCount = 0;
+
+    private Vector3 moveDirection = new Vector3(0, 0, 0);
+    private CharacterController controller;
+    private float mauseInitialPos = 0.0f;
+    private float currentRotation = 0.0f;
+    private float inputAxisX;
+    private float inputAxisY;
+    private float inputAxisZ;
+
+    private
+
+    void Awake()
+    {
+        controller = this.gameObject.GetComponent<CharacterController>();
+        if (!controller)
+        {
+            Debug.LogError("Failed on get the Player controller");
+        }
+
+
+    }
+
+
+    void Update()
+    {
+
+        GetImput(ref inputAxisX,ref inputAxisY,ref inputAxisZ);        
+        Move();
+        ResetJumpCount();
+        Rotate();
+
+
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            mauseInitialPos = Input.mousePosition.x;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            currentRotation = ((Input.mousePosition.x - mauseInitialPos)/(Screen.width/2));
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            currentRotation = 0;
+        }
+
+        transform.Rotate(0, rotateSpeed * currentRotation * Time.deltaTime, 0);
+
+    }
+
+    private void ResetJumpCount()
+    {
+        if (controller.isGrounded)
+        {
+            currentJumpCount = 0;
+        }
+    }
+
+    private void Move()
+    {
+        moveDirection = new Vector3(inputAxisX, 0, inputAxisZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        if (!controller.isGrounded)
+            inputAxisY -= gravity * Time.deltaTime;
+
+        moveDirection.y = inputAxisY;
+
+        controller.Move(moveDirection*Time.deltaTime);
+    }
+
+    private void GetImput(ref float inputX,ref float inputY,ref float inputZ)
+    {
+
+        inputX = Input.GetAxis("Horizontal");
+
+        inputZ = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+        {            
+                Jump(ref inputY);
+        }
+
+
+    }
+
+    private void Jump(ref float inputY)
+    {
+        if (currentJumpCount < maxJumpCap)
+        {
+            inputY = jumpSpeed;
+            currentJumpCount++;
+        }
+    }
+}
---
+++ catQuestChoto/Assets/RigidBodyMovement.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyMovement : MonoBehaviour {
+
+
+    [SerializeField]
+    private float rotateSpeed = 6.0f;
+    [SerializeField]
+    private float speed = 6.0f;
+    [SerializeField]
+    private float jumpSpeed = 8.0f;
+    [SerializeField]
+    private float gravity = 20.0f;
+    [SerializeField]
+    private int maxJumpCap = 1;
+
+    private int currentJumpCount = 0;
+
+    private Vector3 moveDirection = new Vector3(0, 0, 0);
+    private Rigidbody rb;
+    private float mauseInitialPos = 0.0f;
+    private float currentRotation = 0.0f;
+    private float inputAxisX;
+    private float inputAxisY;
+    private float inputAxisZ;
+    
+
+    private
+
+    void Awake()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        if (!rb)
+        {
+            Debug.LogError("Failed on get the Player rigidbody");
+        }
+
+
+    }
+
+    private void Update()
+    {
+        GetImput(ref inputAxisX, ref inputAxisZ);
+        ResetJumpCount();
+    }
+
+    void FixedUpdate()
+    {         
+        Move();        
+        Rotate();
+    }
+
+    private bool isGrounded()
+    {
+        bool answer = Physics.Raycast(transform.position, Vector3.down, 1);
+        return answer;
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            mauseInitialPos = Input.mousePosition.x;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            currentRotation = ((Input.mousePosition.x - mauseInitialPos)/(Screen.width/2));
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            currentRotation = 0;
+        }
+        Quaternion deltaRotation = Quaternion.Euler(0, rotateSpeed * currentRotation * Time.fixedDeltaTime, 0);
+        rb.MoveRotation(rb.rotation*deltaRotation);
+
+    }
+
+    private void ResetJumpCount()
+    {
+        if(isGrounded())
+        {
+            currentJumpCount = 0;
+        }
+    }
+
+    private void Move()
+    {
+        moveDirection = new Vector3(inputAxisX, 0, inputAxisZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        rb.MovePosition(this.transform.position + (moveDirection * Time.fixedDeltaTime));
+
+     }
+
+    private void GetImput(ref float inputX,ref float inputZ)
+    {
+
+        inputX = Input.GetAxis("Horizontal");
+
+        inputZ = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+        {            
+                Jump();
+        }
+
+
+    }
+
+    private void Jump()
+    {
+        if (currentJumpCount < maxJumpCap)
+        {
+            rb.AddForce(Vector3.up * jumpSpeed,ForceMode.Impulse);
+            currentJumpCount++;
+        }
+    }
+}
