c12fdfedb96bd7ef00460c62d1b4621edd89f214
Esteban <Esteban_127@hotmail.com>
2018-05-30 11:54:23 -0300

    EnemyAnimations

---
+++ catQuestChoto/Assets/HitManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitManager : MonoBehaviour {
+
+    private void OnTriggerStay(Collider collide)
+    {
+        if(collide.tag == "Enemy")
+        {
+
+            Debug.Log("holiWacho");
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
