8b6b7c0d8169781688af1bb74d48ac50fc5b6d4d
Croutier <Croutier127@gmail.com>
2018-08-30 17:13:50 -0300

    Add abilities base

---
+++ catQuestChoto/Assets/Scripts/Abilties/AtackAbility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AtackAbility : IAbility {
+
+    [SerializeField] float abilityDamage = 1; 
+    
+
+    public override bool TryCastAbility(GameObject target)
+    {
+        if(target.tag == "Enemy")
+        {
+            if ((target.transform.position - this.gameObject.transform.position).magnitude < range && remainCooldown <= 0)
+            {
+                CastAbility(target.GetComponent<IACTOR>())
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected override void CastAbility(IACTOR target)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
---
+++ catQuestChoto/Assets/Scripts/Abilties/IAbility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IAbility : MonoBehaviour {
+
+    [SerializeField] protected Sprite abilitySprite;
+    [SerializeField] protected float range;
+    [SerializeField] float baseCooldown;
+    protected float cooldown;
+    protected float remainCooldown = 0;
+
+    public abstract bool TryCastAbility(GameObject target);
+    protected abstract void CastAbility(IACTOR target);
+
+    private void calculateColdown()
+    {
+        //should calculate final coldown w/ buffs and stuff
+        cooldown = baseCooldown;
+    }
+
+
+    private void Start()
+    {
+        cooldown = baseCooldown;
+    }
+
+    private void Update()
+    {
+        if (remainCooldown > 0)
+            remainCooldown -= Time.deltaTime;
+    }
+}
