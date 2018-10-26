aea02822168234efbb5310d210fe8575987165c4
Esteban <Esteban_127@hotmail.com>
2018-10-26 12:10:55 -0300

    dialogue manager first insatnce

---
+++ catQuestChoto/Assets/Scripts/Actor/NpcIneraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DialogueManager))]
+public class NpcIneraction : MonoBehaviour {
+
+    //questmanager
+    DialogueManager dialogue;
+
+
+    private void Start()
+    {
+        //instanciate el questmanajor
+        dialogue = GetComponent<DialogueManager>();
+    }
+    public void Interact()
+    {
+        /*
+            string key = "Base";
+            chequeo en el quest manager
+            interface de dialogo
+            for (int i = 0; i < questKey.lengt; i++)
+            {
+                if(dialogue.CheckKey(questKey[i]))
+                    key = questKey[i];
+            }
+            dialogue.getDialogue(key)
+        */
+    }
+}
---
+++ catQuestChoto/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    [SerializeField] string[] dialogueTree;
+    Dictionary<string, int> dictionary;
+    [SerializeField] string[] dKey;
+    private void Start()
+    {
+        dictionary = new Dictionary<string, int>(dKey.Length);
+        for (int i = 0; i < dKey.Length; i++)
+        {
+            dictionary.Add(dKey[i], i);
+        }
+    }
+
+    public string getDialogue(string key)
+    {
+        return dialogueTree[dictionary[key]];        
+    }
+
+    public bool CheckKey(string key)
+    {
+        return (dictionary.ContainsKey(key));
+    }
+}
